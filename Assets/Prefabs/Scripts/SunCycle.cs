29e620ef0a95c26202ba8068a126d315f434006f
Miagkostup_Igor <justbl0w1991@gmail.com>
2020-07-12 15:38:56 +0300

    ingame art prototype scene created

---
+++ Assets/Prefabs/Scripts/SunCycle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunCycle : MonoBehaviour
+{
+    [SerializeField] float speed = 0.3f;
+    
+
+   
+    void Update()
+    {
+        this.transform.Rotate(0, this.transform.rotation.y + speed * Time.deltaTime,0);
+    }
+}
